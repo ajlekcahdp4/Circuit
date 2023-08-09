7856c5a53cc47f8670356a508b5448bf8e32555a
LegendaryHog <kuteinikov.sd@phystech.edu>
2023-08-09 20:44:37 +0300

    practicing in bad designing

--- include/circuit.hpp
+++ include/circuit.hpp
@@ -1,31 +1,35 @@
 #include <vector>
 #include <pair>
-#include <memory>
+#include <unordered_map>
 #include <iostream>
+#include <algorithm>
+#include <memory>
+#define DEBUG
 
 namespace Circuit {
 
+namespace detail 
+{
 class Edge;
-using Node = std::pair<int, std::vector<Edge*>>;
+using Node = std::pair<std::size_t, std::vector<Edge*>>;
 
 class Edge
 {
-    using node_ptr   = Node*;
-    using c_node_ptr = const Node*;
-
-    node_ptr node1_ = nullptr, node2_ = nullptr;
+    std::size_t node_num1_ = 0, node_num2_ = 0;
     double resistance_ = 0.0, emf_ = 0.0;
     double voltage_ = 0.0, current_ = 0.0;
 
 public:
-    Edge(node_ptr n1, node_ptr node2, double r, double e)
-    :node1_ {n1}, node2_ {n2}, resistance_ {r}, emf_ {e}
+    Edge(std::size_t n1, std::size_t n2, double r, double e)
+    :node_num1_ {n1}, node_num2_ {n2}, resistance_ {r}, emf_ {e}
     {}
 
-    node_ptr   node1()       {return node1_;}
-    c_node_ptr node1() const {return node1_;}
-    node_ptr   node2()       {return node2_;}
-    c_node_ptr node2() const {return node2_;}
+    Edge(std::size_t n1, std::size_t n2, double r)
+    :node_num1_ {n1}, node_num2_ {n2}, resistance_ {r}
+    {}
+
+    std::size_t node_num1() const {return node_num1_;}
+    std::size_t node_num2() const {return node_num2_;}
 
     double resistance() const {return resistance_;}
     double emf() const {return emf_;}
@@ -35,18 +39,37 @@ public:
     double voltage() const {return voltage_;}
     double current() const {return current_;}
 
-    bool is_valid() const {return node1_ != node2_;}
+    bool is_valid() const {return node_num1_ != node_num2_;}
 }; // class Edge
+} // namespace detail
 
 class Circuit
 {
-    using node_ptr = Node*;
+    using Node = detail::Node;
+    using Edge = detail::Edge;
 
-    std::vector<Node> nodes_;
+    std::unordered_map<std::size_t, Node> nodes_ = {};
+    std::vector<std::unique_ptr<Edge>>    edges_ = {};
 
 public:
-    Circuit(std::istream& input): nodes_ {}
+    Circuit() = default;
 
+    bool add_node(std::size_t node_num)
+    {
+        return nodes_.emplace(node_num, node, std::vector<Edge*>()).second;
+    }
+
+    bool add_edge(std::size_t n1, std::size_t n2, double resistance, double emf)
+    {
+        auto pedge = edges_.emplace_back(std::make_unique<Edge>(n1, n2, resistance, emf)).get();
+        auto& itr1 = nodes_.find(n1);
+        auto& itr2 = nodes_.find(n2);
+
+        if (itr1 == nodes_.end() || itr2 == nodes_.end())
+            return false;
+        itr1->second.second.push_back(pedge);
+        itr2->second.second.push_back(pedge);
+        return true;
+    }
 }; // class Circuit
-
 } // namespace Circuit
