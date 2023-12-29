2d3e2dba0d061fce76cd87b16043ee6d40ee1f0a
LegendaryHog <kuteinikov.sd@phystech.edu>
2023-12-29 12:27:58 +0300

    remove ind form edge

--- include/edge.hpp
+++ include/edge.hpp
@@ -6,11 +6,11 @@ namespace Circuit
 {
 struct Edge
 {
-    unsigned node1_ = 0, node2_ = 0, ind_ = 0;
+    unsigned node1_ = 0, node2_ = 0;
     double resistance_ = 0.0, emf_ = 0.0;
     Edge() = default;
-    Edge(unsigned n1, unsigned n2, unsigned ind, double r, double e = 0.0)
-    :node1_ {n1}, node2_ {n2}, ind_ {ind}, resistance_ {r}, emf_ {e}
+    Edge(unsigned n1, unsigned n2, double r, double e = 0.0)
+    :node1_ {n1}, node2_ {n2}, resistance_ {r}, emf_ {e}
     {}
 }; // struct Edge
 
@@ -23,6 +23,6 @@ struct std::hash<Circuit::Edge>
     std::size_t operator()(const Circuit::Edge& edge) const noexcept
     {
         std::hash<unsigned> hash;
-        return hash(edge.node1_) ^ hash(edge.node2_) ^ hash(edge.ind_);
+        return hash(edge.node1_) ^ hash(edge.node2_);
     }
 };
--- src/edge.cpp
+++ src/edge.cpp
@@ -4,6 +4,6 @@ namespace Circuit
 {
 bool operator==(const Edge& e1, const Edge& e2)
 {
-    return e1.node1_ == e2.node1_ && e1.node2_ == e2.node2_ && e1.ind_ == e2.ind_;
+    return e1.node1_ == e2.node1_ && e1.node2_ == e2.node2_;
 }
 } // namespace Circuit
